71d7bb911f58ad4fb8e4f1b97b17b6d33bd264f0
zohaib-crypto <khanzaibi191@gmail.com>
2025-11-10 21:14:28 +0000

    Update DialogManager.cs

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 using TMPro;
 public class DialogManager : MonoBehaviour
 {
-    [SerializeField]
-    private TMP_Text dialogText;
-    [SerializeField]
-    private GameObject dialogBox;
+    [SerializeField] private TMP_Text dialogText;
+    [SerializeField] int lettersPerSecond;
+    [SerializeField] private GameObject dialogBox;
     public void ShowDialog()
     {
-
+        dialogBox.SetActive(True);
+        StartCoroutine(TypeDialog());
+    }
+    public IEnumerator TypeDialog(string line)
+    {
+        dialogText.text = "";
+        foreach (var letter in line.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(1f / lettersPerSecond);
+        }
     }
 }
